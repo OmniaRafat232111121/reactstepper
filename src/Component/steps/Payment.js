ec4c5592c1a1e6cdc30953d145c062f2933b31fa
mohamed magdy ismael <mohamed.magdy@vezeeta.com>
2023-02-28 13:04:21 +0200

    Localize payment

--- src/Component/steps/Payment.js
+++ src/Component/steps/Payment.js
@@ -1,6 +1,7 @@
 import { useStepperContext } from "../../contexts/StepperContext";
 
-export default function Payment() {
+export default function Payment({ ...props }) {
+  const { localization } = props;
   const { userData, setUserData } = useStepperContext();
 
   const handleChange = (e) => {
@@ -11,28 +12,28 @@ export default function Payment() {
     <div className="flex flex-col ">
       <div className="w-full mx-2 flex-1">
         <div className="font-bold h-6 mt-3 text-gray-500 text-xs leading-8 uppercase">
-          Credit Card
+          {localization.creditCard}
         </div>
         <div className="bg-white my-2 p-1 flex border border-gray-200 rounded">
           <input
             onChange={handleChange}
             value={userData["card"] || ""}
             name="card"
-            placeholder="Credit Card#"
+            placeholder={`#${localization.creditCard}`}
             className="p-1 px-2 appearance-none outline-none w-full text-gray-800"
           />
         </div>
       </div>
       <div className="w-full mx-2 flex-1">
         <div className="font-bold h-6 mt-3 text-gray-500 text-xs leading-8 uppercase">
-          Exp
+        {localization.exp}
         </div>
         <div className="bg-white my-2 p-1 flex border border-gray-200 rounded">
           <input
             onChange={handleChange}
             value={userData["exp"] || ""}
             name="exp"
-            placeholder="YY/MM/DD"
+            placeholder={localization.dateFormat}
             type="text"
             className="p-1 px-2 appearance-none outline-none w-full text-gray-800"
           />
