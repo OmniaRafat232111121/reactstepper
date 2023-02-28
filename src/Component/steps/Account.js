48c67749e25f12991d3519f93e5c19ca558bd7af
mohamed magdy ismael <mohamed.magdy@vezeeta.com>
2023-02-28 12:34:18 +0200

    Localize account step

--- src/Component/steps/Account.js
+++ src/Component/steps/Account.js
@@ -1,8 +1,8 @@
 import { useStepperContext } from "../../contexts/StepperContext";
 
-export default function Account() {
+export default function Account({ ...props }) {
+  const { localization } = props;
   const { userData, setUserData } = useStepperContext();
-
   const handleChange = (e) => {
     const { name, value } = e.target;
     setUserData({ ...userData, [name]: value });
@@ -12,28 +12,28 @@ export default function Account() {
     <div className="flex flex-col ">
       <div className="mx-2 w-full flex-1">
         <div className="mt-3 h-6 text-xs font-bold uppercase leading-8 text-gray-500">
-          Username
+          {localization.username}
         </div>
         <div className="my-2 flex rounded border border-gray-200 bg-white p-1">
           <input
             onChange={handleChange}
             value={userData["username"] || ""}
             name="username"
-            placeholder="Username"
+            placeholder={localization.username}
             className="w-full appearance-none p-1 px-2 text-gray-800 outline-none"
           />
         </div>
       </div>
       <div className="mx-2 w-full flex-1">
         <div className="mt-3 h-6 text-xs font-bold uppercase leading-8 text-gray-500">
-          Password
+        {localization.password}
         </div>
         <div className="my-2 flex rounded border border-gray-200 bg-white p-1">
           <input
             onChange={handleChange}
             value={userData["password"] || ""}
             name="password"
-            placeholder="Password"
+            placeholder={localization.password}
             type="password"
             className="w-full appearance-none p-1 px-2 text-gray-800 outline-none"
           />
