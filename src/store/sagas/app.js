f5650f4106b368e5a3bd6b3faae235c9f8ab4daf
mohamed magdy ismael <mohamed.magdy@vezeeta.com>
2023-02-28 12:13:13 +0200

    Set redux store

---
+++ src/store/sagas/app.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable require-yield */
+import { takeEvery, all, put } from 'redux-saga/effects';
+import { APP } from '../actions/app';
+
+function* test(payload) {
+  console.log(payload)
+}
+
+export default function* actionsSaga() {
+  yield all([
+    takeEvery(APP.SET_APP_LANGUAGE, test),
+  ]);
+}
